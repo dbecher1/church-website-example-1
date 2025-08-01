efb5b56ef3351c4328fcc2223925b0b7f80e941c
Daniel Becher <debecher93@gmail.com>
2025-07-31 21:21:48 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,137 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Logo from '$lib/assets/logo.svelte';
+	import { Icon, Bars3, XMark } from 'svelte-hero-icons';
+	import { fade, slide } from 'svelte/transition';
+
+	let { children } = $props();
+
+	let show = $state(false);
+	const toggleShow = () => show = !show;
+
+	const links = [
+		{text: 'About', href: '/'},
+		{text: 'Sermons', href: '/'},
+		{text: 'Visit', href: '/'},
+		{text: 'Events', href: '/'},
+		{text: 'Give', href: '/'},
+	]
+
+	const footer_links = [
+		{title: 'About Us', links: [
+			{text: 'Our Story', href: ''},
+			{text: 'Staff', href: ''},
+			{text: 'Beliefs', href: ''},
+			{text: 'Ministries', href: ''},
+			{text: 'Legal Items', href: ''},
+		]},
+		{title: 'Learn More', links: [
+			{text: 'Giving', href: ''},
+			{text: 'Service', href: ''},
+			{text: 'Community Outreach', href: ''},
+			{text: 'Missions', href: ''},
+			{text: 'Sponsor a Child', href: ''},
+		]},
+		{title: 'Contact Us', links: [
+			{text: '1990 Hill Ave, Columbus, OH, 12345', href: ''},
+			{text: 'church@the.hill', href: 'mailto:church@the.hill'},
+			{text: '555-123-4567'}
+		]}
+	]
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<svelte:window on:wheel|nonpassive={e => {
+	if (show) e.preventDefault();
+}}/>
+
+{#snippet sidebar()}
+{#if show}
+	<div role={'figure'} class='absolute top-0 left-0 bg-black/60 w-dvw h-dvh z-50' transition:fade onclick={toggleShow}></div>
+	<div class='absolute top-0 right-0 h-dvh w-[45dvw] min-w-64 z-51 bg-base-100 flex p-6' transition:slide={{axis: 'x'}}>
+		<div class='flex flex-col w-full mx-auto p1-'>
+			<div class='flex flex-row justify-between'>
+				<div class='flex flex-row gap-2 p-1'>
+					<Logo class='size-8 fill-primary inline'/>
+					<div class='text-lg mt-1'>The Hill</div>
+				</div>
+				<button onclick={toggleShow}>
+					<Icon src={XMark} class='btn btn-ghost btn-sm px-1 fill-base-content'/>
+				</button>
+			</div>
+			<nav class='flex flex-col p-2'>
+			{#each [{text: 'Home', href: '/'}, ...links] as {text, href}}
+				<a {href} class='link link-hover py-1 text-lg' onclick={toggleShow}>{text}</a>
+			{/each}
+		</nav>
+		</div>
+		
+	</div>
+{/if}
+{/snippet}
+
+{#snippet header()}
+<nav class='grid grid-cols-3 w-full py-6 mx-auto'>
+	
+	<div class='mx-auto'>
+		<a href='/'>
+			<Logo class='fill-primary h-10 lg:h-12'/>
+		</a>
+	</div>
+	
+	<div class='flex justify-center'>
+		<nav class='not-md:hidden flex gap-4'>
+			{#each links as {text, href}}
+				<a {href} class='btn btn-ghost btn-lg px-4 py-1 hover:text-primary whitespace-nowrap'>{text}</a>
+			{/each}
+		</nav>
+	</div>
+
+	
+	<div class='mx-auto'>
+		<button class='md:hidden btn btn-ghost btn-lg hover:text-primary' onclick={toggleShow}>
+			Menu <Icon src={Bars3} class='size-6 lg:size-8 ml-1'/>
+		</button>
+	</div>
+	
+</nav>
+
+{/snippet}
+
+{#snippet footer()}
+	<div class='footer sm:footer-horizontal py-10 px-14'>
+		{#each footer_links as {title, links}}
+			<nav>
+				<h4 class='footer-title'>{title}</h4>
+				{#each links as {text, href}}
+					{#if href}
+						<a {href} class='link link-hover'>{text}</a>
+					{:else}
+						<div class='link link-hover'>{text}</div>
+					{/if}
+				{/each}
+			</nav>
+		{/each}
+	</div>
+	<aside class='text-[9pt] flex flex-col px-14 py-8'>
+		<div>&copy; 2025, The Hill, All Rights Reserved.</div>
+		<div>Website by Daniel Becher.</div>
+	</aside>
+{/snippet}
+
+<!-- CONTENT -->
+
+{@render sidebar()}
+<header class='max-w-[120rem] w-full mx-auto'>
+	{@render header()}
+</header>
+<main class='flex-1 max-w-[120rem] w-full mx-auto relative flex flex-col overflow-x-hidden'>
+	{@render children?.()}
+</main>
+<footer class='flex flex-col bg-neutral text-neutral-content'>
+	{@render footer()}
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    import Logo from '$lib/assets/logo.svelte';
+
+    const services_ministries = [
+        ['Sunday Worship', ['9 AM, 11 AM Sunday Morning']],
+        ['Young Adults Ministry', ['7 PM Sunday Evening']],
+        ["Ladies' Breakfast Study", ['10 AM, Tuesday Morning']],
+        ['Middle School Youth', ['6 PM Wednesday Evening']],
+        ['High School Youth', ['7 PM, Friday Evening']]
+    ]
+</script>
+
+<svelte:head>
+    <title>The Hill | Welcome Home</title>
+</svelte:head>
+
+{#snippet homepage_hero()}
+    <div class='mt-6 mb-12 lg:my-20 w-full max-w-sm sm:max-w-md md:max-w-2xl lg:max-w-4xl mx-auto uppercase text-center flex flex-col'>
+        <div class='py-0 text-3xl md:text-5xl/20 lg:text-7xl/22 font-bold'>
+            Jesus <span class='text-primary'>loved</span> his community. <br class='md:hidden'>We do <span class='text-primary'>too.</span>
+        </div>
+        <div class='text-xl md:text-2xl lg:text-4xl font-semibold py-8 tracking-tight'>
+            We are <span class='text-primary'>The Hill.</span>
+        </div>
+        <div class=''>
+            <a href='/' class='btn btn-primary btn-outline not-md:btn-sm lg:btn-lg rounded-box'>Welcome Home</a>
+        </div>
+    </div>
+{/snippet}
+
+{#snippet homepage_bg()}
+    <Logo class='w-5/12 absolute -z-1 top-0 left-1/2 -translate-x-1/2 fill-neutral/5'/>
+    <Logo class='not-md:hidden w-5/12 absolute -z-1 top-0 left-1/12 -translate-x-1/2 fill-neutral/5'/>
+    <Logo class='not-md:hidden w-5/12 absolute -z-1 top-0 left-11/12 -translate-x-1/2 fill-neutral/5'/>
+{/snippet}
+
+{#snippet info()}
+    <div class='w-full bg-primary relative text-primary-content px-4 py-12 lg:p-24 grid grid-cols-1 lg:grid-cols-2 gap-8'>
+        <Logo class='w-5/12 absolute z-1 top-0 left-1/2 lg:left-1/4 -translate-x-1/2 fill-primary-content/5 rotate-180'/>
+        <Logo class='not-lg:hidden w-5/12 absolute z-1 top-0 left-3/4 -translate-x-1/2 fill-primary-content/5 rotate-180'/>
+        <div class='text-3xl md:text-5xl/20 lg:text-7xl/22 font-bold z-2 text-center lg:text-right px-12'>
+            At The Hill, we strive to love like <span class='text-neutral/90 bg-primary-content px-3'>Jesus.</span>
+        </div>
+        <div class='not-lg:hidden z-2  row-span-2'>
+            <img src='/northview1.webp' alt='A woman worshipping in a purple sanctuary.' class='opacity-90'/>
+        </div>
+        <div class='text-xl md:text-2xl lg:text-4xl font-semibold z-2 text-center lg:text-right px-12'>
+            He said, <span class='uppercase'>"come as you are,"</span> so no matter your history or walk, we would love for you to encounter this love.
+        </div>
+        <div class='mx-auto px-12 grid grid-cols-2'>
+            <a href='/' class='col-start-3 btn btn-base-100 btn-outline lg:btn-lg rounded-sm font-bold text-xl'>Learn More</a>
+        </div>
+    </div>
+    <div class='w-full bg-base-200 text-base-content p-4 mx-auto grid grid-cols-1 lg:grid-cols-2 relative'>
+        <div class='not-lg:hidden p-24'>
+            <img src='/worship.jpg' alt='A Worship and praise service.' class='opacity-90'/>
+        </div>
+        <div class='grid grid-cols-3 pb-8 px-12 lg:px-16 lg:py-8 gap-4 lg:gap-0'>
+            <div class='text-2xl lg:text-4xl col-span-3 text-center place-items-center p-4 lg:p-8'>
+                Services and Ministries
+            </div>
+            <hr class='lg:hidden col-span-3 opacity-30 -mt-4'>
+            {#each services_ministries as [service, time]}
+                <div class='col-span-3 lg:col-span-2 not-lg:text-center text-xl lg:p-2'>{service}</div>
+                <div class='col-span-3 lg:col-span-1 not-lg:text-center lg:text-lg p-1 lg:p-2'>{time}</div>
+                <hr class='lg:hidden col-span-3 opacity-30'>
+            {/each}
+        </div>
+    </div>
+{/snippet}
+
+<!-- CONTENT -->
+{@render homepage_bg()}
+{@render homepage_hero()}
+{@render info()}
